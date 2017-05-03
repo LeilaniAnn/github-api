8fc2490dec0ced0713d6c175b1d6de2d651e5c8c
leilani <leilani.ann.raranga@gmail.com>
2017-05-03 16:19:23 -0700

    feat: api endpoints added

--- app.py
+++ app.py
@@ -1,14 +1,44 @@
-import os
 
-from flask import Flask
-from flask import render_template
+import os
+import requests
+import json
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        username = request.form['username']
+        # because I exceeded my limit, lets make it authenticated
+        resp = requests.get("https://api.github.com/users/"+ username +"/keys?state=closed&access_token=28204bde5c3dc023967893e7717f92d59c53bc3e")
+        print resp.status_code
+        response_dict = resp.json()
+        if resp.status_code != 200:
+            error = "Username not found"
+            return render_template('index.html', error=error)
+
+        else:
+            return render_template('index.html', username=username,response_dict=response_dict)
+
+@app.route('/<username>', methods=['GET'])
+def show_user_profile(username):
+    resp = requests.get("https://api.github.com/users/"+ username +"/keys?state=closed&access_token=28204bde5c3dc023967893e7717f92d59c53bc3e")
+    print resp.status_code
+    response_dict = resp.json()
+    if resp.status_code != 200:
+        error = "Username not found"
+        return render_template('profile.html', error=error)
+
+    else:
+        return render_template('profile.html', username=username,response_dict=response_dict)
+
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
