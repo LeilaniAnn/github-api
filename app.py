0a4b39b938e79d3d33d573956451b7805747a90b
leilani <leilani.ann.raranga@gmail.com>
2017-05-03 17:50:25 -0700

    upon making repo public, github said NO because of access token

--- app.py
+++ app.py
@@ -14,9 +14,11 @@ def home():
         return render_template('index.html')
     else:
         username = request.form['username']
-        # because I exceeded my limit, lets make it authenticated
-        resp = requests.get("https://api.github.com/users/"+ username +"/keys?state=closed&access_token=28204bde5c3dc023967893e7717f92d59c53bc3e")
+        # in case you exceed your limit
+        # +"/keys?state=closed&access_token=ACCESS_TOKEN")
+        resp = requests.get("https://api.github.com/users/"+ username
         response_dict = resp.json()
+        print response_dict
         if resp.status_code != 200:
             error = "Username not found"
             return render_template('index.html', error=error)
@@ -26,7 +28,9 @@ def home():
 
 @app.route('/<username>', methods=['GET'])
 def show_user_profile(username):
-    resp = requests.get("https://api.github.com/users/"+ username +"/keys?state=closed&access_token=28204bde5c3dc023967893e7717f92d59c53bc3e")
+    # in case you exceed your limit
+    # +"/keys?state=closed&access_token=ACCESS_TOKEN")
+    resp = requests.get("https://api.github.com/users/"+ username + '"')
     response_dict = resp.json()
     if resp.status_code != 200:
         error = "Username not found"
